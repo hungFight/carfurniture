efdbb31a4769e69a05a360877064aba12715f413
Tran Thanh Dat <dat.tt.2041>
2023-12-25 19:24:45 +0700

    Thanh Dat

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const nextConfig = {
         DB_URL: process.env.DB_URL_SERVER,
         DEFAULT_PRODUCT: process.env.DEFAULT_PRODUCT_c,
     },
-    distDir: 'build',
-    output: 'standalone'
 }
 
 module.exports = nextConfig
